9d63a8756aa45d3ebb632b5eb2cd9ec5872a42e1
MTBakalov18 <MTBakalov18@codingburgas.bg>
2023-05-02 13:04:29 +0300

    кумит

--- src/MyMagazine.java
+++ src/MyMagazine.java
@@ -1,8 +1,9 @@
 public class MyMagazine extends Magazine {
+    private String title;
 
-
-    public MyMagazine(String name, String author, double price) {
+    public MyMagazine(String name, String author, double price, String title) {
         super(name, author, price);
+        this.title = title;
     }
 
     @Override
